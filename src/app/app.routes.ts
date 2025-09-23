5661a606130b7ba0f0203efa53c505d8ccfeabf3
Threshika V S <threshika.vs@theproindia.com>
2025-09-23 14:34:26 +0530

    Routing setup- navigation between the components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProductList } from './components/product-list/product-list';
+import { AddProduct } from './components/add-product/add-product';
+import { Order } from './components/order/order';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //default route
+    {path: '', redirectTo:'product-list',pathMatch:'full'},
+    
+    {path: 'product-list', component: ProductList},
+    {path: 'add-product', component: AddProduct},
+    {path: 'order', component: Order}
+];
