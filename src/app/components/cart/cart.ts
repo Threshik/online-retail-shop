c3d1e4245ef464f939b426454ab268d7f03fc9f1
Threshik <threshikavijay@gmail.com>
2025-10-10 17:09:57 +0530

    cart->keycloak

--- src/app/components/cart/cart.ts
+++ src/app/components/cart/cart.ts
@@ -1,19 +1,20 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
 import { CartItem } from '../../models/cart-item.model';
+import { AuthHttpService } from '../../services/auth-http';
 
 @Component({
   selector: 'app-cart',
   imports: [FormsModule],
   templateUrl: './cart.html',
-  styleUrl: './cart.css'
+  styleUrls: ['./cart.css']
 })
 export class Cart implements OnInit {
-  http = inject(HttpClient)
-  router = inject(Router)
+  private authHttp = inject(AuthHttpService);
+  private router = inject(Router);
+
   cartItems: CartItem[] = [];
 
   ngOnInit(): void {
@@ -21,57 +22,48 @@ export class Cart implements OnInit {
   }
 
   showCartItems() {
-    this.http.get<CartItem[]>(`${environment.apiBaseUrl}/cart`).subscribe({
+    this.authHttp.get<CartItem[]>(`${environment.apiBaseUrl}/cart`).subscribe({
       next: (res) => {
         this.cartItems = res;
       },
       error: (err) => {
-        console.log("Cart Items couldnt fetch");
-
+        console.log("Cart Items couldn't fetch", err);
       }
-    })
+    });
   }
 
   updateQuantity(cartItemId: number, newQuantity: number) {
-    this.http.put(`${environment.apiBaseUrl}/cart/update`, {
-      cartItemId,
-      quantity: newQuantity
-    }).subscribe({
+    this.authHttp.put(`${environment.apiBaseUrl}/cart/update`, { cartItemId, quantity: newQuantity }).subscribe({
       next: () => alert('Quantity updated!'),
       error: (err) => console.error('Failed to update quantity', err)
     });
   }
 
   deleteCartItem(id: number) {
-    this.http.delete(`${environment.apiBaseUrl}/cart/${id}`).subscribe({
+    this.authHttp.delete(`${environment.apiBaseUrl}/cart/${id}`).subscribe({
       next: () => {
-        alert('Cart Item deleted!')
+        alert('Cart Item deleted!');
         this.showCartItems();
       },
       error: (err) => console.error('Failed to remove item', err)
-
-    })
+    });
   }
 
   clearCart() {
-    this.http.delete(`${environment.apiBaseUrl}/cart/clear`).subscribe({
+    this.authHttp.delete(`${environment.apiBaseUrl}/cart/clear`).subscribe({
       next: () => {
-        alert("Cart cleared Successfully")
+        alert('Cart cleared Successfully');
         this.showCartItems();
       },
-      error: (err) => {
-        console.error('Failed to clear cart ', err)
-      }
-    })
+      error: (err) => console.error('Failed to clear cart', err)
+    });
   }
 
   navigate() {
-
-    this.router.navigate(['order'])
-
-
+    this.router.navigate(['order']);
   }
 
+  //getters
   get totalItems() {
     return this.cartItems.length;
   }
